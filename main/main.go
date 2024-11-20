73a0c27b2781e0f17442ca2af47fb63f2d16038c
gavraz <gavra@gmail.com>
2024-11-20 23:29:46 +0200

    Simple main example

--- main/main.go
+++ main/main.go
@@ -4,44 +4,19 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 	"strings"
 )
 
 func main() {
 	app := Command{
 		Name:        "my-cli",
-		Usage:       "my-cli farewell|greet",
+		Usage:       "my-cli greet",
 		Description: "prints",
 		Action: func(ctx Flags, args []string) error {
 			fmt.Println("Welcome to my cli!")
 			return nil
 		},
 		Subcommands: []*Command{
-			{
-				Name:        "farewell",
-				Description: "Says goodbye",
-				Flags: []Flag{
-					BoolFlag{
-						Name:     "NICE",
-						Required: true,
-						Default:  false,
-					},
-					StringFlag{
-						Name:     "STR",
-						Required: false,
-					},
-				},
-				Action: func(ctx Flags, args []string) error {
-					addition := ctx.String("STR")
-					if ctx.Bool("NICE") {
-						fmt.Println("You are my farewell!", addition)
-					} else {
-						fmt.Println("Goodbye!", addition)
-					}
-					return nil
-				},
-			},
 			{
 				Name:        "greet",
 				Usage:       "greet --name NAME",
@@ -72,59 +47,11 @@ func main() {
 		},
 	}
 
-	addCmd := &Command{
-		Name:        "add",
-		Usage:       "add A B",
-		Description: "calculates A + B",
-		Action: func(ctx Flags, args []string) error {
-			x, err := strconv.Atoi(args[0])
-			if err != nil {
-				return err
-			}
-			y, err := strconv.Atoi(args[1])
-			if err != nil {
-				return err
-			}
-			fmt.Println("sum is:", x+y)
-			return nil
-		},
-	}
-	subCmd := &Command{
-		Name:        "sub",
-		Usage:       "sub A B",
-		Description: "calculates A - B",
-		Action: func(ctx Flags, args []string) error {
-			x, err := strconv.Atoi(args[0])
-			if err != nil {
-				return err
-			}
-			y, err := strconv.Atoi(args[1])
-			if err != nil {
-				return err
-			}
-			fmt.Println("diff is:", x-y)
-			return nil
-		},
-	}
-
-	calcCmd := &Command{
-		Name:        "calc",
-		Usage:       "calc [add|sub] X Y",
-		Description: "calculates X op Y",
-		Subcommands: []*Command{addCmd, subCmd},
-		Action: func(ctx Flags, args []string) error {
-			// Run subcommand
-			return nil
-		},
-	}
-	//calcCmd.AddSubCommand(addCmd)
-	//calcCmd.AddSubCommand(subCmd)
-	app.AddSubcommand(calcCmd)
-
-	fmt.Println(os.Args)
-	// Run the application
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Input:	greet --name Taylor --shout true
+	// Output:	HELLO, TAYLOR!
 }
